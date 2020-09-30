164bde44466e7e5b4f500f8533cc3596ecc435c4
Adonis Emmanuel <67901350+AdonisAE@users.noreply.github.com>
2020-09-30 17:35:56 +0100

    Add files via upload

---
+++ Player/Interact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public bool pressed = false;
+    // Update is called once per frame
+    void Update()
+    {
+        //IF the user presses Q
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            //Quit the game
+            pressed = true;
+        }
+        else
+        {
+            pressed = false;
+        }
+
+
+    }
+}
